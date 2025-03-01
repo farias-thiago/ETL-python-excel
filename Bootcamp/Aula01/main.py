caef0bcd0a18b64e0c8077c2144a64cc5a36d8b4
Thiago Farias <thiagofarias.ti@gmail.com>
2025-03-01 00:31:38 -0300

    Add Desafio

--- Bootcamp/Aula01/main.py
+++ Bootcamp/Aula01/main.py
@@ -4,3 +4,10 @@ num1 = int(input("Digite um número: "))
 num2 = int(input("Digite outro número: "))
 print(f"A soma dos números é: {num1 + num2}")
 
+# Desafio 1
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+
+salario = float(input("Digite seu salário: "))
+bonus = float(input("Digite o valor do bônus: "))
+print(f"O salário total é: {salario + bonus + 1000}")
